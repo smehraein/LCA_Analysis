c80c829ee258fd2597573d67e1436f4e69a1217d
Soroush Mehraein <soroushmehraein@gmail.com>
2015-05-14 00:09:35 -0700

    Commit 2: Electric Boogaloo
    Created an IPython Notebook to keep track of progress and data
    exploration. Optimized initial filtering of LCA data (it’s not actually
    H1B applications - important distinction). Combined “Clean_Up.py” and
    “Initial_Filter.py” into one file and removed code and comments related
    to exploring the data. All data exploration will be done in the
    notebooks. The .py files will are there to quickly generate the end
    product of each step (in this case, a filtered and prepped cvs file).

--- Initial_Filter.py
+++ Initial_Filter.py
@@ -2,45 +2,45 @@ __author__ = 'soroushmehraein'
 
 import pandas as pd
 
-# Massive amount of data here. Need to filter out stuff ASAP for usability.
-# Also convert to CSV for easier access. Need "utf-8" encoding, however.
-# Room for improvement with Hadoop to use multiple year's worth of data?
 
-f = open("2014_H1B.xlsx", "r")
+# Open file and read to dataframe.
+f = open("2014_LCA.xlsx", "r")
 
-print "reading"
+print "Reading"  # This takes a while...
 raw_df = pd.read_excel(f)
-
+print "Done Reading"
 f.close()
 
-"""
-From looking at the raw data and my own intuition, I assume that the following columns are not needed:
-"VISA_CLASS" - These are all H1B
-"OTHER_WAGE_SOURCE_1" - Wage info source
-"LCA_CASE_WORKLOC2_CITY" - 2nd location info is generally very sparse. Will ignore.
-"LCA_CASE_WORKLOC2_STATE"
-"PW_2"
-"PW_UNIT_2"
-"PW_SOURCE_2"
-"OTHER_WAGE_SOURCE_2"
-"YR_SOURCE_PUB_2"
 
-"""
-unwanted_columns = ["VISA_CLASS", "OTHER_WAGE_SOURCE_1", "LCA_CASE_WORKLOC2_CITY", "LCA_CASE_WORKLOC2_CITY",
-                    "LCA_CASE_WORKLOC2_STATE", "PW_2", "PW_UNIT_2", "PW_SOURCE_2", "OTHER_WAGE_SOURCE_2",
-                    "YR_SOURCE_PUB_2"]
+# Drop unwanted columns.
+unwanted_columns = ["VISA_CLASS", "LCA_CASE_WAGE_RATE_TO", "PW_SOURCE_1", "OTHER_WAGE_SOURCE_1", "YR_SOURCE_PUB_1",
+                    "LCA_CASE_WORKLOC2_CITY", "LCA_CASE_WORKLOC2_CITY", "LCA_CASE_WORKLOC2_STATE",
+                    "PW_2", "PW_UNIT_2", "PW_SOURCE_2","OTHER_WAGE_SOURCE_2", "YR_SOURCE_PUB_2"]
 
 raw_df = raw_df.drop(unwanted_columns, axis=1)
 
-# Furthermore, let's remove rows with STATUS = WITHDRAWN.
-# These rows provide no useful classification data.
+# Drop na rows.
+raw_df = raw_df.dropna()
 
-raw_df = raw_df[raw_df.STATUS != "WITHDRAWN"]
+# Truncate ZIP codes to 5 numbers.
+raw_df["LCA_CASE_EMPLOYER_POSTAL_CODE"] = [str(t)[:5] for t in raw_df["LCA_CASE_EMPLOYER_POSTAL_CODE"]]
 
+# Remove formatting from EMPLOYER_NAME column.
+raw_df["LCA_CASE_EMPLOYER_NAME"] = raw_df["LCA_CASE_EMPLOYER_NAME"].str.replace(",", "")  # remove ","
+raw_df["LCA_CASE_EMPLOYER_NAME"] = raw_df["LCA_CASE_EMPLOYER_NAME"].str.replace(".", "")  # remove "."
+raw_df["LCA_CASE_EMPLOYER_NAME"] = raw_df["LCA_CASE_EMPLOYER_NAME"].str.replace(" - ", "-")  # remove whitespace
+raw_df["LCA_CASE_EMPLOYER_NAME"] = raw_df["LCA_CASE_EMPLOYER_NAME"].str.replace("  ", " ")  # remove double spaces
 
-# On the flip-side, STATUS = CERTIFIED-WITHDRAWN is just as useful as CERTIFIED
+# Update status column.
+raw_df = raw_df[raw_df.STATUS != "INVALIDATED"]
+raw_df = raw_df.replace("REJECTED", "DENIED")
 
-raw_df = raw_df.replace("CERTIFIED-WITHDRAWN", "CERTIFIED")
+# Remove rows with obvious errors.
+raw_df = raw_df[raw_df.PW_1 != 0]
+raw_df = raw_df[raw_df.TOTAL_WORKERS != 2013]
 
-print "Writing"
-raw_df.to_csv("Filtered_H1B.csv", encoding="utf-8")
+# Drop TOTAL_WORKERS column
+raw_df = raw_df.drop("TOTAL_WORKERS", axis=1)
+
+# Write to .csv
+raw_df.to_csv("Prepped_LCA.csv", encoding="utf-8")
